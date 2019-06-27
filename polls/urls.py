1c29e74c5689c7174faafaeca0ae79d5366e3701
dev1-responsify <dev1@responsify.com>
2019-06-27 10:56:08 -0400

    added api views

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
 
 #The Routing that calls the polls_list and polls_detail method
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
